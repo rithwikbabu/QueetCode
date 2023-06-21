82346af097485aee03ec1a21c715ba71c2cd0ce3
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-06-21 16:37:36 -0500

    fixed markdown file bugs

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -18,21 +18,20 @@ const Markdown: React.FC<MarkdownProps> = ({ md }) => {
                 <code
                   className="rounded-[5px] border border-[#f7faff1f]
                             bg-[#ffffff12] p-[.125rem] text-xs leading-4 text-[#eff1f6bf]"
+                  children={children}
                   {...props}
-                >
-                  {children}
-                </code>
+                />
               );
             }
             // The code block inside a pre tag will just render the children
-            return <>{children}</>
+            return <>{children}</>;
           },
-          p: ({ node, ...props }) => <p className="mb-4" {...props} />,
-          li: ({ node, ...props }) => <li className="mb-3" {...props} />,
-          ul: ({ node, ...props }) => <ul className="mb-4 mx-4" {...props} />,
-          pre: ({ node, ...props }) => (
+          p: ({ ...props }) => <p className="mb-4" {...props} />,
+          li: ({ ...props }) => <li className="mb-3" {...props} />,
+          ul: ({ ...props }) => <ul className="mx-4 mb-4" {...props} />,
+          pre: ({ ...props }) => (
             <pre
-              className="bg-[#ffffff12] text-[#eff1f6bf] rounded-lg text-sm leading-5 my-4 p-4 whitespace-pre-wrap"
+              className="my-4 whitespace-pre-wrap rounded-lg bg-[#ffffff12] p-4 text-sm leading-5 text-[#eff1f6bf]"
               {...props}
             />
           ),
