59977d6785b2ad2cfa20ad490da5abf68f679e7e
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-06-22 09:28:57 -0500

    fixed linter error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,17 +19,22 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  // extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  // rules: {
+  //   "@typescript-eslint/consistent-type-imports": [
+  //     "warn",
+  //     {
+  //       prefer: "type-imports",
+  //       fixStyle: "inline-type-imports",
+  //     },
+  //   ],
+  //   "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  // },
+  extends: "next",
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-  },
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
 };
 
 module.exports = config;
