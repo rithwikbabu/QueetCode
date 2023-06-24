8fff09cb6f40a7113810bc9f4f0945fffba29428
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-06-24 09:39:44 -0500

    fixed build errors 2

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,15 +1,19 @@
-export function unescapeString(escapedString: string): string {
-  // Use JSON.parse to handle most escape sequences.
-  // We wrap the string in quotes to make it valid JSON.
-  let unescapedString = JSON.parse(`"${escapedString}"`);
-
-  // JSON.parse does not handle \xHH sequences, so we handle them manually.
-  unescapedString = unescapedString.replace(
-    /\\x([0-9A-Fa-f]{2})/g,
-    function (_: any, p1: string) {
-      return String.fromCharCode(parseInt(p1, 16));
+export function unescapeString(escapedString: string): string | null {
+    // Wrap the string in quotes to make it valid JSON.
+    try {
+      // Try to parse the string as JSON to handle most escape sequences.
+      let unescapedString: string = JSON.parse(`"${escapedString}"`);
+  
+      // JSON.parse does not handle \xHH sequences, so we handle them manually.
+      unescapedString = unescapedString.replace(
+        /\\x([0-9A-Fa-f]{2})/g,
+        (_match: string, p1: string) => String.fromCharCode(parseInt(p1, 16))
+      );
+  
+      return unescapedString;
+    } catch (e) {
+      console.error('Error unescaping string: ', e);
+      return null;
     }
-  );
-
-  return unescapedString;
-}
+  }
+  
